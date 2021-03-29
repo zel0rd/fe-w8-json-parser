94b3522e34790cc9d336cd30b9eef54ffc1a0b8a
younggil <zelord.kwoun@gmail.com>
2021-03-29 16:33:14 +0900

    [ADD] Inital Code Upload

---
+++ builder/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+//여기도 추가
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    mode: "development",
+    entry: ['./src/app.js', './src/index.css' ],
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            }, 
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            }
+            ,
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin(
+        {
+            template: 'src/app.html'
+        }
+        
+    )]
+}
