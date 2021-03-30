16bf951826b1b4b68efe1d85c94033f05bd2a8d3
Hannah Park <ha3158987@gmail.com>
2021-03-30 16:47:28 +0900

    [ADD] create the tokenizer

---
+++ builder/src/js/lexer.js
@@ -0,0 +1,5 @@
+//의미를 분석. value랑 type을 분류해서 객체형태로 저장해줌.
+const lexer = () => {}
+
+
+export default lexer;
---
+++ builder/src/js/main.js
@@ -0,0 +1,27 @@
+//-------------module import------------
+import { pipe } from './util.js';
+import tokenizer from './tokenizer.js';
+import lexer from './lexer.js';
+import parser from './parser.js';
+
+//--------------test cases--------------
+const ex1 = '[1,2,[3,4,[5,[6]]]]';
+const ex2 = '[1, [2,[3]],"hello world", "codesquadFE", null]';
+const ex3 = '"1a3",[null,false,["11",[112233],{"easy" : ["hello", {"a":"a"}, "world"]},112],55, "99"],{"a":"str", "b":[912,[5656,33],{"key" : "innervalue", "newkeys": [1,2,3,4,5]}]}, true';
+
+//json parser를 구동.
+function JSONParser (str) {
+    console.log("시험용");
+    // const result = pipe(tokenizer, lexer, parser)(str);
+    // return result;
+    console.log(tokenizer(str));
+}
+
+const runTestCases = (str) => {
+    JSONParser(str);
+    // console.dir(JSONParser(ex1), {depth: null});
+    // console.dir(JSONParser(ex2), {depth: null});
+    // console.dir(JSONParser(ex3), {depth: null});
+}
+
+export default runTestCases;
