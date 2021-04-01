2bc797b915fc2b8d23811c8860b2db98c19fe6be
Hannah Park <ha3158987@gmail.com>
2021-04-01 12:03:56 +0900

    [REFACTOR] fix bug in view by changing css property

--- builder/src/app.js
+++ builder/src/app.js
@@ -12,8 +12,6 @@ const ex3 = '["1a3",[null,false,["11",[112233],{"easy" : ["hello", {"a":"a"}, "w
 
 const submitBtn = _.$(".submit");
 const outputStr = _.$(".output");
-// let inputStr = _.$("#story").value;
-
 const result = {
     tokenizer: _.$(".tokenizer_viewer"),
     lexer: _.$(".lexer_viewer"),
@@ -25,8 +23,7 @@ submitBtn.addEventListener("click", () => {
     if(isValidate(inputStr)){
         result.tokenizer.textContent = JSON.stringify(tokenizer(inputStr));
         result.lexer.textContent = JSON.stringify(lexer(tokenizer(inputStr)));
-        result.parser.textContent = JSON.stringify(parser(lexer(tokenizer(inputStr))), null, " ");
-        // outputStr.textContent = JSON.stringify(runTestCases(inputStr));
+        result.parser.textContent = JSON.stringify(parser(lexer(tokenizer(inputStr))), null, "   ");
     } else {
         result.tokenizer.textContent = "Invalid Input";
         result.lexer.textContent = "Invalid Input";
